2acfc4e12e034824c8d0c1a4f7e44bffefe5868f
MJL502 <97486980+MJL502@users.noreply.github.com>
2022-10-04 13:42:47 -0400

    Create KnowledgeCheckOne.py

---
+++ KnowledgeCheckOne.py
@@ -0,0 +1,29 @@
+# a statement that prints "hello world!"
+print("Hello World")
+
+# a list populated with several values. They can be strings / integers / floats etc. 
+myList = ["chocolate", "bacon", "steak"]
+
+# Then, print() one of those values either to the prompt or in a cell (if using Jupyter Notebook)
+print(MyList[0])
+
+# a dictionary populated with two keys and two values. 
+newDict = {
+  "do": "a deer, a female deer",
+  "re": "a drop of golden sun",
+  "mi": "a name, I call myself",
+  "fa": "a long, long way to run",
+  "so": "a needle pulling thread",
+  "la": "a note to follow so",
+  "ti": "a drink with jam and bread"
+}
+
+# Print one of the values just like you did above.
+print(newDict[do])
+
+# a tuple with 4 values. Print one of them.
+planetaryMnemonicTuple = ("My", "Very", "Entertaining", "Monkey", "Just", "Sat", "Upon", "9", "Peanuts")
+print(planetaryMnemonicTuple[3])
+
+#Commit your changes.
+#Push your changes to your repo and notify your mentor!
